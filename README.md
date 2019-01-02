6a8440f2e77e0fd047c64230ef0e3314bbef7685
顾重 <gucx@dxy.cn>
2019-01-02 11:42:33 +0800

    test: 测试发布

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ### 顾重 订阅号 自动回复服务
 
+[![Build Status](https://travis-ci.com/guchongxi/gucx_subscribe.svg?branch=master)](https://travis-ci.com/guchongxi/gucx_subscribe)
+
 ### 技术栈
 * [node](http://nodejs.cn/api/)
 * [express](https://expressjs.com/en/4x/api.html)
--- deploy.sh
+++ deploy.sh
@@ -20,5 +20,7 @@ fi
 
 # docker run -d -p 8181:8181 --name $3 --env-file .env $1:$2
 echo -e "\nRun container..."
-docker run -d -p 8181:8181 --name $3 $1:$2
+# $4 pm2 secret key
+# $4 pm2 pm2 public key
+docker run -d -p 8181:8181 --name $3 -e PM2_SECRET_KEY=$4 -e PM2_PUBLIC_KEY=$5 -e PM2_MACHINE_NAME=$3 $1:$2
 echo "Run container success🎉"
