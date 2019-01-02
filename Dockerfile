978dc8cf3601b8b82a69f25c9693fe1556d494d3
顾重 <gucx@dxy.cn>
2019-01-02 12:41:50 +0800

    feat: 增加部署完成测试

--- Dockerfile
+++ Dockerfile
@@ -8,16 +8,11 @@ ENV NODE_ENV=production \
 
 WORKDIR /usr/src/app
 
-# Bundle APP files
 COPY . .
 
-# Install app dependencies
 RUN npm install --production --slient \
-  # Show current folder structure in logs
-  # -a, –all 列出目录下的所有文件，包括以 . 开头的隐含文件
   && ls -a
 
-# Expose the listening port of your app
 EXPOSE 8181
 
 CMD [ "pm2-runtime", "start", "ecosystem.config.js" ]
--- deploy.sh
+++ deploy.sh
@@ -18,9 +18,25 @@ else
 	echo "No local container"
 fi
 
-# docker run -d -p 8181:8181 --name $3 --env-file .env $1:$2
 echo -e "\nRun container..."
 # $4 pm2 secret key
 # $4 pm2 pm2 public key
 docker run -d -p 8181:8181 --name $3 -e PM2_SECRET_KEY=$4 -e PM2_PUBLIC_KEY=$5 -e PM2_MACHINE_NAME=$3 $1:$2
-echo "Run container success🎉"
+echo "Run container success"
+
+HOST_NAME=https://weixin.guchongxi.com
+
+# -s Silent mode (don't output anything)
+# -m Maximum time allowed for the transfer
+# --connect-timeout Maximum time allowed for connection
+# -I Show document info only
+info=$(curl -s -m 10 --connect-timeout 10 -I $HOST_NAME)
+
+#获取返回码
+code=$(echo $info | grep "HTTP" | awk '{print $2}')
+#对响应码进行判断
+if [ "$code" == "200" ]; then
+	echo "Deploy success🎉"
+else
+	echo "Deploy fail"
+fi
